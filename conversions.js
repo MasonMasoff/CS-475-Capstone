548f080d99299a4e46da38db6bf858aab1fe0a4a
MasonMasoff <masongarcia8100@gmail.com>
2022-02-17 13:45:02 -0600

    binaryToDecimal for a single instance is working

--- conversions.js
+++ conversions.js
@@ -5,10 +5,31 @@ function textToBinary(string) {
     }).join(' ');
 }
 
-// Function that takes binary as input, outputs a text conversion
+// Function that takes binary as input, outputs a decimal conversion
+ function binaryToDecimal(string) {
+    let decimal = +0;
+    let bits = +1;
+    for(let i = 0; i < string.length; i++) {
+        let currNum = +(string[string.length - i - 1]);
+        if(currNum === 1) {
+            decimal += bits;
+        }
+        bits *= 2;
+    }
+    return(decimal);
+}
 
+// NEED to have the binaryToDecimal be able to take in multiple strings for more than one number
+// EX: 1001000 1100101 1101100 1101100 1101111
+// EX: 72 101 108 108 111
 
-
-
+// Tests
+//      Text to Binary
 console.log('Text To Binary:');
-console.log(textToBinary('Hello'))
+console.log(textToBinary('Hello'));
+
+console.log('\n');
+
+//      Binary to Decimal
+console.log('Binary to Decimal:');
+console.log(binaryToDecimal('1001000'));
