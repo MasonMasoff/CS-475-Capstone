61bb855791a839ad96a30b812e270622c555f0e2
MasonMasoff <masongarcia8100@gmail.com>
2022-02-24 13:33:13 -0600

    find function returns index of stop character

--- Decode.js
+++ Decode.js
@@ -5,10 +5,18 @@ function toArray(str, delim) {
     return arr = str.split(',');
 }
 
-function decodeString(string) {
-    var arr = toArray(string, ',');
+/*    find    */
+//  Returns index of stop character
+function find(arr) {
+    const stp = '11111111';
+    var index = arr.indexOf(stp);
 
-    console.log(arr);
+    return index;
+}
+
+function decodeString(str) {
+    var arr = toArray(str, ',');
+    console.log(find(arr));
 }
 
 decodeString('01001001,01100100,01101111,01101101,01101101,00101110,00100001,01010101,11111111,01101111,01110010,01101100,01100100')
